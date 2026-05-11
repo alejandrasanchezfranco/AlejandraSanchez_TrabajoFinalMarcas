4f20ef6d9a9e89ffcfd8b2a6686255b6ce5d4a8f
Alejandra Sánchez <alejandrasanchezfranco@gmail.com>
2026-05-11 17:52:12 +0200

    Recurso secundario
    Hoy he creado las operaciones básicas del recurso secundario y las he guardado en Bruno

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ app.use(express.json());
 
 let fotos = [
     {
-        id:1,
-        titulo: "Surf en Cádiz",
-        categoria: "surf",
-        ubicacion: "Cádiz",
-        fecha: "2026-02-17",
-        autor:"Mi padre",
-        camara: "Canon",
-        rating: 4.5,
-        favorita: true
+        "id":1,
+        "titulo": "Surf en Cádiz",
+        "categoria": "surf",
+        "ubicacion": "Cádiz",
+        "fecha": "2026-02-17",
+        "autor":"Mi padre",
+        "camara": "Canon",
+        "rating": 4.5,
+        "favorita": true
     },
     {
-        id:2,
-        titulo: "Flamenco en Cádiz",
-        categoria: "flamenco",
-        ubicacion: "Cádiz",
-        fecha: "2025-11-16",
-        autor: "Mi padre",
-        camara: "Canon",
-        rating: 4.5,
-        favorita:false
+        "id":2,
+        "titulo": "Flamenco en Cádiz",
+        "categoria": "flamenco",
+        "ubicacion": "Cádiz",
+        "fecha": "2025-11-16",
+        "autor": "Mi padre",
+        "camara": "Canon",
+        "rating": 4.5,
+        "favorita":false
     }
 ];
 let comentarios = [
@@ -51,11 +51,12 @@ app.listen(port, () => {
 
 //====== Endpoints básicos ========
 
+//Obtener todas las fotos
 app.get('/fotos',(req,res)=>{
     res.json(fotos);
 });
 
-// Obtener una foto por id
+// Obtener una foto por id (route param)
 app.get('/fotos/:id',(req,res)=>{
     const foto = fotos.find(f => f.id == req.params.id);
 
@@ -66,8 +67,11 @@ app.get('/fotos/:id',(req,res)=>{
 });
 
 //Otra forma de obtener una foto ( query param)
-app.get('/foto',(req,res)=>{
+app.get('/buscar-foto',(req,res)=>{
     const { id } = req.query;
+    if(!id){
+        return res.status(400).json({error:"Parámetro 'id' requerido"});
+    }
 
     const foto = fotos.find(f=> f.id == id);
 
@@ -80,16 +84,29 @@ app.get('/foto',(req,res)=>{
 //Crear foto (POST)
 
 app.post('/fotos',(req,res)=>{
-    const nueva = req.body;
 
-    if(!nueva.titulo || !nueva.categoria){
+   
+
+    if(!req.body.titulo || !req.body.categoria|| !req.body.ubicacion || !req.body.fecha || !req.body.autor || !req.body.camara || !req.body.rating || !req.body.favorita){
         return res.status(400).json({ error: "Faltan campos obligatorios"});
     }
-    nueva.id = fotos.length + 1;
+    
+    let nuevaFoto = {
+        id:fotos.length + 1,
+        titulo: req.body.titulo,
+        categoria: req.body.categoria,
+        ubicacion: req.body.ubicacion,
+        fecha: req.body.fecha,
+        autor:req.body.autor,
+        camara: req.body.camara,
+        rating: req.body.rating,
+        favorita: req.body.favorita
+    }
+    
 
-    fotos.push(nueva);
+    fotos.push(nuevaFoto);
 
-    res.status(201).json(nueva);
+    return res.status(201).json(nuevaFoto);
 });
 
 //Modificar la foto (PUT)
@@ -100,8 +117,8 @@ app.put('/fotos/:id',(req,res)=>{
     if (index === -1){
         return res.status(404).json ({error: "Foto no encontrada"});
     }
-    if(!req.body|| Object.keys(req.body).lenght === 0){
-        return res.status(400).json({error: "Nose enviaron datos"});
+    if(!req.body|| Object.keys(req.body).length === 0){
+        return res.status(400).json({error: "No se enviaron datos"});
     }
 
     fotos[index] = {...fotos[index],...req.body};
@@ -124,21 +141,52 @@ app.delete('/fotos/:id',(req,res)=>{
 
 //======== Endpoints recursos secundarios =========
 
-//Obtener todos los registros secundarios
+//Obtener todos los comentarios
 
 app.get('/comentarios',(req,res)=>{
     res.json(comentarios);
 });
 
-//Obtener registros secundarios que pertenecen a un principal concreto
+//Obtener comentarios de una foto específica
 
 app.get('/fotos/:id/comentarios',(req,res)=>{
     const fotoId = req.params.id;
     const comentariosFoto = comentarios.filter(c => c.foto_id == fotoId);
-    res.json(comentariosFoto);
-})
 
-//Crear un nuevo r.secundario
+    if(comentariosFoto.length === 0){
+        return res.status (404).json({error: "No hay comentarios para esta foto"});
+    }
+    res.json(comentariosFoto);
+});
+
+//Crear un nuevo comentario ( POST)
+
+app.post('/comentarios',(req,res) =>{
+    
+    if(!req.body.foto_id|| !req.body.usuario || !req.body.texto || !req.body.fecha){
+        return res.status (400).json({error:"Faltan campos obligatorios"});
+    }
+
+    const fotoExiste = fotos.find(f => f.id == req.body.foto_id);
+    if(!fotoExiste){
+        return res.status(404).json({error:"La foto no existe"});
+    }
+
+    const nuevoComentario = {
+        id:comentarios.length +1,
+        foto_id :req.body.foto_id,
+        usuario :req.body.usuario,
+        texto :req.body.texto,
+        fecha :req.body.fecha
+    };
+
+    comentarios.push(nuevoComentario);
+    res.status(201).json(nuevoComentario);
+});
+
+
+
+
 
 
 
